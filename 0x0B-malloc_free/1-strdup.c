959876ee108fc67e438e8175b8c30e943ac7c928
ICONHUEY <iconhuey@gmail.com>
2022-10-03 17:58:16 -0700

    compiles

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - copies a string into a new buffer
+ * @str: the string to copy
+ *
+ * Return: pointer to a new buffer
+ */
+char *_strdup(char *str)
+{
+	int i, end;
+	char *array;
+
+	if (str == NULL)
+		return (NULL);
+	for (end = 0; end <= *str; end++)
+	{
+	}
+
+	end += 1;
+	array = malloc(sizeof(char) * end);
+
+	for (i = 0; i < end; i++)
+		array[i] = str[i];
+
+	if (array == NULL)
+		return (NULL);
+	return (array);
+}
